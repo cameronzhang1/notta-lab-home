8b5224cbee4858beb6414b3e91a72a8fed9a876e
cameronzhang1 <52635505+cameronzhang1@users.noreply.github.com>
2023-07-31 15:40:21 -0400

    mvp branch type changes

--- pages/components/navitem.jsx
+++ pages/components/navitem.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const NavItem = ({ text, href, active }) => {
+const NavItem = ({ text, href }) => {
   return (
     <Link href={href} scroll={false}>
         <h2 className= "nav__link">{text}</h2>
